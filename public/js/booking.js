4df3700ac8e19cba1f710efd33ad314a2b574bb0
Joe <freela532@gmail.com>
2022-01-09 13:29:51 -0400

    Select selected new

--- public/js/booking.js
+++ public/js/booking.js
@@ -125,13 +125,7 @@ Invoice.prototype = {
     },
 
     newRow: function () {
-        jQuery(".item-row:last").after('<tr class="item-row"><td><div class="delete-btn"><input class="form-control qty" name="quantity[]" id="quantity[]" value="1" type="text" readonly="readonly"><a class=' + $.opt.delete.substring(1) + ' href="javascript:;" title="Remove row">X</a></div></td><td class="col-4"><select class="form-control" name="service_id[]" id="service_id" onchange=\'changePrice();\'></select></td><td><input class="form-control date" type="text" name="date[]" id="datepicker" placeholder="Select" onfocus="(this.type=\'date\')" onblur="(this.type=\'text\')"></td><td><select class="form-control" name="time[]"><option value="" selected>Select</option><option value="06:00">06:00</option><option value="06:30">06:30</option><option value="07:00">07:00</option><option value="07:30">07:30</option><option value="08:00">08:00</option><option value="08:30">08:30</option><option value="09:00">09:00</option><option value="09:30">09:30</option><option value="10:00">10:00</option><option value="10:30">10:30</option><option value="11:00">11:00</option><option value="11:30">11:30</option><option value="12:00">12:00</option><option value="12:30">12:30</option><option value="13:00">13:00</option><option value="13:30">13:30</option><option value="14:00">14:00</option><option value="14:30">14:30</option><option value="15:00">15:00</option><option value="15:30">15:30</option><option value="16:00">16:00</option><option value="16:30">16:30</option><option value="17:00">17:00</option><option value="17:30">17:30</option><option value="18:00">18:00</option><option value="18:30">18:30</option><option value="19:00">19:00</option><option value="19:30">19:30</option><option value="200:00">20:00</option></select></td><td><input class="form-control price" name="price[]" id="price[]" placeholder="Precio" type="number"></td><td><span class="total">0.00</span></td></tr>');
-		
-        if (jQuery($.opt.delete).length > 0) {
-            jQuery($.opt.delete).show();
-        }
 
-        return 1;
     },
 
     deleteRow: function (elem) {
